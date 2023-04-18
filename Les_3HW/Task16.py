d81b6f806771073ea757cddcd4cceb8db57fc937
Stepan <stepanmaslov@gmail.com>
2023-04-18 13:52:19 +0300

    add Les3_HW

---
+++ Les_3HW/Task16.py
@@ -0,0 +1,15 @@
+﻿
+# Задача 16
+lens = int(input("введите длинну: "))
+arr = ""
+
+for temp in range(lens):
+    arr += input("введите элемент: ")
+
+
+foundNumb = input("введите искомый элемент: ")
+count = 0
+for temp in arr:
+    count = count + 1 if (temp == foundNumb) else count
+
+print(f"Мы имеем {count} элементов {foundNumb}")
