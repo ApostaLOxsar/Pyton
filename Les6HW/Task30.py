d5c33ed32c02467816305e658666759a62f64b92
Stepan <Stepanmaslov@gmail.com>
2023-04-30 14:05:03 +0300

    add Task6HW

---
+++ Les6HW/Task30.py
@@ -0,0 +1,15 @@
+﻿#Задача 30:  Заполните массив элементами арифметической прогрессии. 
+#Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
+#Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+
+
+firstNumber = int(input("Введите первый элемент: "))
+diferent = int(input("Введите разность: "))
+countNumber = int(input("Введите количество элементов: "))
+
+arr = list()
+arr.append(firstNumber)
+for i in range(1,countNumber):
+    arr.append(arr[i -1] * diferent)
+
+print(arr)
