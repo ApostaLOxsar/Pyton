e0a34b4d5607d3cea40d36516633e0ca23197ff2
Stepan <stepanmaslov@gmail.com>
2023-05-26 13:11:51 +0300

    add 7hw

---
+++ Les7HW/task34.py
@@ -0,0 +1,21 @@
+﻿
+#Task34
+
+def ritm(inputString):
+    inputString = inputString.split()
+    countVowel = []
+    for temp in inputString:
+        tempCount = 0
+        for tempChar in temp:
+            if tempChar in 'йуеыаоэяию':
+                tempCount += 1
+        countVowel.append(tempCount)
+    return len(countVowel) == countVowel.count(countVowel[0])
+
+
+
+inputStr = input("Введите строку: ")
+if (ritm(inputStr)):
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
---
+++ Les7HW/task36.py
@@ -0,0 +1,9 @@
+﻿#task36
+
+def print_operation_table(f, num_rows, num_columns):
+    res = [[f(i, j) for i in range(1, num_columns + 1)] for j in range(1, num_rows + 1)]
+         #[[operation(i, j) for i in range(1, num_columns + 1)] for j in range(1, num_raws + 1)]
+    for i in res:
+        print(i)
+
+print_operation_table(lambda x, y: x * y, 6, 6)
